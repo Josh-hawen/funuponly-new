4aa4e3cc1e8ccebb0df8eea86126bf13cb01ec39
Josh-hawen <josh@i.ua>
2018-04-01 19:45:46 +0300

    Archpage
    Archpage fuo-jh added.
    reworked logo on body slider

--- js/main.js
+++ js/main.js
@@ -24,6 +24,92 @@ $('.fa-times').click(function() {
 	$(this).parent().toggleClass("hidden");
 })
 
+////////////////////////////////////////////////////
+
+$('#fun22-prev').click(function() {
+	$('.fun22').toggleClass('active');  
+	$('.fun21p2').toggleClass('active');
+}) 
+
+$('#fun21p2-next').click(function() {
+	$('.fun22').toggleClass('active');  
+	$('.fun21p2').toggleClass('active');
+}) 
+
+$('#fun21p2-prev').click(function() { 
+	$('.fun21p2').toggleClass('active');
+	$('.fun21p2').toggleClass('non-active');
+	$('.fun21p1').toggleClass('active');
+}) 
+
+$('#fun21p1-next').click(function() { 
+	$('.fun21p2').toggleClass('active');
+	$('.fun21p2').toggleClass('non-active');
+	$('.fun21p1').toggleClass('active');
+}) 
+
+$('#fun21p1-prev').click(function() { 
+	$('.fun21p1').toggleClass('active');
+	$('.fun21p1').toggleClass('non-active');
+	$('.fun20').toggleClass('active');
+}) 
+
+$('#fun20-next').click(function() { 
+	$('.fun21p1').toggleClass('active');
+	$('.fun21p1').toggleClass('non-active');
+	$('.fun20').toggleClass('active');
+}) 
+
+$('#fun20-prev').click(function() { 
+	$('.fun20').toggleClass('active');
+	$('.fun20').toggleClass('non-active');
+	$('.fun19').toggleClass('active');
+}) 
+
+$('#fun19-next').click(function() { 
+	$('.fun20').toggleClass('active');
+	$('.fun20').toggleClass('non-active');
+	$('.fun19').toggleClass('active');
+}) 
+
+$('#fun19-prev').click(function() { 
+	$('.fun19').toggleClass('active');
+	$('.fun19').toggleClass('non-active');
+	$('.fun18').toggleClass('active');
+}) 
+
+$('#fun18-next').click(function() { 
+	$('.fun19').toggleClass('active');
+	$('.fun19').toggleClass('non-active');
+	$('.fun18').toggleClass('active');
+}) 
+
+$('#fun18-prev').click(function() { 
+	$('.fun18').toggleClass('active');
+	$('.fun18').toggleClass('non-active');
+	$('.fun17').toggleClass('active');
+}) 
+
+$('#fun17-next').click(function() { 
+	$('.fun18').toggleClass('active');
+	$('.fun18').toggleClass('non-active');
+	$('.fun17').toggleClass('active');
+}) 
+
+$('#fun17-prev').click(function() { 
+	$('.fun17').toggleClass('active');
+	$('.fun17').toggleClass('non-active');
+	$('.fun16').toggleClass('active');
+}) 
+
+$('#fun16-next').click(function() { 
+	$('.fun17').toggleClass('active');
+	$('.fun17').toggleClass('non-active');
+	$('.fun16').toggleClass('active');
+}) 
+
+
+
 });
 
 
